036f240f90d6e89cb29d9f9b37179ff5f72d7924
Mallikarjun-64 <kgoudar.ms@gmail.com>
2026-07-21 20:57:44 +0530

    update Achievements

--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -1,11 +1,7 @@
 import { useEffect } from "react";
 import AchievementItem from "../components/AchievementItem";
 import { achievementData } from "../data/achievementsData";
-import { ImageList, ImageListItem } from "@mui/material";
-import { useMediaQuery } from "@mui/material";
-
 function Achievements() {
-  const isMobile = useMediaQuery("(max-width: 900px)");
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -16,18 +12,18 @@ function Achievements() {
         <h2 className="h2 article-title">Achievements</h2>
       </header>
 
-      <ImageList variant="masonry" cols={isMobile ? 1 : 2} gap={"1.7rem"}>
+      <div style={{ display: "flex", flexDirection: "column", gap: "2.5rem" }}>
         {achievementData.map((data) => (
-          <ImageListItem key={data.title}>
+          <div key={data.title}>
             <AchievementItem
               images={data.images}
               title={data.title}
               date={data.date}
               desc={data.desc}
             />
-          </ImageListItem>
+          </div>
         ))}
-      </ImageList>
+      </div>
     </article>
   );
 }
